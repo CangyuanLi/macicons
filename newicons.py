fd76fa429f0700eee30912ff949d0119cf3ff5e8
CangyuanLi <everest229@gmail.com>
2021-08-14 21:54:33 -0400

    changed check to be time based

--- newicons.py
+++ newicons.py
@@ -3,6 +3,7 @@ import json
 import os
 from pathlib import Path
 import subprocess
+import time
 
 base_path = Path(__file__).parent
 iconfolder = str(base_path / 'icons')
@@ -21,30 +22,40 @@ def replace_icon(root, path, key):
     subprocess.run(['fileicon', 'set', fullpath, iconpath], stdout=subprocess.DEVNULL)
 
 def replace_all_icons(dumb):
+    changes = 0
     for root, dirs, files in os.walk(root_path, topdown=True):
         for dir in dirs:
             if dir in mapper_dict: # faster then mapper_dict.keys()
                 replace_icon(root=root, path=dir, key=dir)
+                changes += 1
         
         dirs[:] = [d for d in dirs if d not in ignorelist] # don't go through some folders
         
         for file in files:
             if dumb == True:
-                output = 'NO'
+                time_since_creation = 0
             elif dumb == False:
-                output = subprocess.run(['fileicon', 'test', file], capture_output=True).stdout.decode('utf8')
+                time_created = os.path.getctime(os.path.join(root, file))
+                time_since_creation = time.time() - time_created
 
-            if 'NO' in output:
+            if time_since_creation < 100000: # 1 day in unix seconds + a little bit of padding
                 filename, file_ext = os.path.splitext(file)
                 
                 if filename in mapper_dict:
                     replace_icon(root=root, path=file, key=filename)
+                    changes += 1
 
                 if file_ext in mapper_dict:
                     replace_icon(root=root, path=file, key=file_ext)
+                    changes += 1
+
+    return changes
 
 def main():
-    replace_all_icons(dumb=False)
+    start = time.time()
+    changes = replace_all_icons(dumb=False)
+    end = time.time()
+    print(f'Made {changes} changes in {round(end - start, 2)} seconds.')
 
 if __name__ == '__main__':
     main()
