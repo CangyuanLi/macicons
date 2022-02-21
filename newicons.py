607586c80b3697e5b5a59f87358f445365b57280
CangyuanLi <everest229@gmail.com>
2022-02-20 22:41:24 -0500

    add multiprocessing

--- newicons.py
+++ newicons.py
@@ -1,63 +1,76 @@
+# Imports
+
 import argparse
+import functools
 import json
+import multiprocessing
 import os
 from pathlib import Path
 import subprocess
 import time
 
-base_path = Path(__file__).parent
-iconfolder = str(base_path / 'icons')
-root_path = '/Users/cangyuanli/Documents/'
+# Globals
 
-with open(base_path / 'mapper.json') as f:
+BASE_PATH = Path(__file__).parent
+ICONFOLDER = str(BASE_PATH / "icons")
+ROOT_PATH = "/Users/cangyuanli/Documents/Projects/genfunc"
+CORES = multiprocessing.cpu_count()
+
+with open(BASE_PATH / "mapper.json") as f:
     mapper_dict = json.load(f)
 
-with open(base_path / 'ignorelist.txt', 'r') as f:
+with open(BASE_PATH / "ignorelist.txt", "r") as f:
     ignorelist = {line.strip() for line in f}
 
-def replace_icon(root, path, key):
+def replace_icon(root, path, key, iconfolder=ICONFOLDER):
     fullpath = os.path.join(root, path)
     iconpath = os.path.join(iconfolder, mapper_dict[key])
 
-    subprocess.run(['fileicon', 'set', fullpath, iconpath], stdout=subprocess.DEVNULL)
+    subprocess.run(["fileicon", "set", fullpath, iconpath], stdout=subprocess.DEVNULL)
 
-def replace_all_icons(dumb):
-    changes = 0
+def replace_file_icon(file, root, dumb):
+    if dumb == True:
+        time_since_creation = 0
+    elif dumb == False:
+        time_created = os.path.getctime(os.path.join(root, file))
+        time_since_creation = time.time() - time_created
+
+    if time_since_creation < 100000: # 1 day in unix seconds + a little bit of padding
+        filename, file_ext = os.path.splitext(file)
+    
+    if filename in mapper_dict:
+        replace_icon(root=root, path=file, key=filename)
+
+    if file_ext in mapper_dict:
+        replace_icon(root=root, path=file, key=file_ext)
+
+    return
+
+def replace_all_icons(root_path=ROOT_PATH, dumb=True, cores=CORES):
     for root, dirs, files in os.walk(root_path, topdown=True):
         for dir in dirs:
             if dir in mapper_dict: # faster then mapper_dict.keys()
                 replace_icon(root=root, path=dir, key=dir)
-                changes += 1
         
-        dirs[:] = [d for d in dirs if d not in ignorelist] # don't go through some folders
+        # modifies directory list in place
+        # this is needed for os.walk to ignore these directories
+        # when visiting the subsequent subdirectories / files
+        dirs[:] = [d for d in dirs if d not in ignorelist]
+
+        func = functools.partial(replace_file_icon, root=root, dumb=True)
         
-        for file in files:
-            if dumb == True:
-                time_since_creation = 0
-            elif dumb == False:
-                time_created = os.path.getctime(os.path.join(root, file))
-                time_since_creation = time.time() - time_created
+        with multiprocessing.Pool(processes=cores) as pool:
+            pool.map(func, files)
 
-            if time_since_creation < 100000: # 1 day in unix seconds + a little bit of padding
-                filename, file_ext = os.path.splitext(file)
-                
-                if filename in mapper_dict:
-                    replace_icon(root=root, path=file, key=filename)
-                    changes += 1
-
-                if file_ext in mapper_dict:
-                    replace_icon(root=root, path=file, key=file_ext)
-                    changes += 1
-
-    return changes
+    return None
 
 def main():
-    start = time.time()
-    changes = replace_all_icons(dumb=False)
-    end = time.time()
-    print(f'Made {changes} changes in {round(end - start, 2)} seconds.')
+    start = time.perf_counter()
+    replace_all_icons()
+    end = time.perf_counter()
+    print(f"Finished in {round(end - start, 2)} seconds.")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 
