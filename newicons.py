0173fea7465539d14ad76a313f394a3e8035de67
CangyuanLi <everest229@gmail.com>
2022-02-21 13:24:32 -0500

    Walk the directories first and build a list of paths. Also use threads instead of multiprocessing.

--- newicons.py
+++ newicons.py
@@ -1,9 +1,9 @@
 # Imports
 
 import argparse
+import concurrent.futures
 import functools
 import json
-import multiprocessing
 import os
 from pathlib import Path
 import subprocess
@@ -14,7 +14,6 @@ import time
 BASE_PATH = Path(__file__).parent
 ICONFOLDER = str(BASE_PATH / "icons")
 ROOT_PATH = "/Users/cangyuanli/Documents/Projects/genfunc"
-CORES = multiprocessing.cpu_count()
 
 with open(BASE_PATH / "mapper.json") as f:
     mapper_dict = json.load(f)
@@ -22,55 +21,55 @@ with open(BASE_PATH / "mapper.json") as f:
 with open(BASE_PATH / "ignorelist.txt", "r") as f:
     ignorelist = {line.strip() for line in f}
 
-def replace_icon(root, path, key, iconfolder=ICONFOLDER):
-    fullpath = os.path.join(root, path)
-    iconpath = os.path.join(iconfolder, mapper_dict[key])
-
-    subprocess.run(["fileicon", "set", fullpath, iconpath], stdout=subprocess.DEVNULL)
-
-def replace_file_icon(file, root, dumb):
-    if dumb == True:
-        time_since_creation = 0
-    elif dumb == False:
-        time_created = os.path.getctime(os.path.join(root, file))
-        time_since_creation = time.time() - time_created
-
-    if time_since_creation < 100000: # 1 day in unix seconds + a little bit of padding
-        filename, file_ext = os.path.splitext(file)
-    
-    if filename in mapper_dict:
-        replace_icon(root=root, path=file, key=filename)
-
-    if file_ext in mapper_dict:
-        replace_icon(root=root, path=file, key=file_ext)
-
-    return None
-
-def replace_all_icons(root_path=ROOT_PATH, dumb=True, cores=CORES):
-    files_visited = 0
+def walk_directory(root_path=ROOT_PATH):
+    filelist = []
     for root, dirs, files in os.walk(root_path, topdown=True):
-        func = functools.partial(replace_file_icon, root=root, dumb=True)
         for dir in dirs:
-            if dir in mapper_dict: # faster then mapper_dict.keys()
-                replace_icon(root=root, path=dir, key=dir)
-        
+            if dir in mapper_dict:
+                filelist.append(os.path.join(root, dir))
+
         # modifies directory list in place
         # this is needed for os.walk to ignore these directories
         # when visiting the subsequent subdirectories / files
         dirs[:] = [d for d in dirs if d not in ignorelist]
-        
-        with multiprocessing.Pool(processes=cores) as pool:
-            pool.map(func, files)
+        filelist += [os.path.join(root, f) for f in files]
 
-        files_visited += len(files)
+    return filelist
 
-    return files_visited
+def replace_icon(filepath, dumb, iconfolder=ICONFOLDER):
+    if dumb == True:
+        time_since_creation = 0
+    elif dumb == False:
+        time_created = os.path.getctime(filepath)
+        time_since_creation = time.time() - time_created
+
+    if time_since_creation < 100000: # 1 day in unix seconds + a little bit of padding
+        basename = os.path.basename(filepath)
+        filename, file_ext = os.path.splitext(basename)
+
+    if file_ext in mapper_dict:
+        iconpath = os.path.join(iconfolder, mapper_dict[file_ext])
+        subprocess.run(["fileicon", "set", filepath, iconpath], stdout=subprocess.DEVNULL)
+    elif filename in mapper_dict:
+        iconpath = os.path.join(iconfolder, mapper_dict[filename])
+        subprocess.run(["fileicon", "set", filepath, iconpath], stdout=subprocess.DEVNULL)
+
+    return None
+
+def replace_all_icons(filelist, dumb=True):
+    func = functools.partial(replace_icon, dumb=dumb)
+
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        pool.map(func, filelist)
+
+    return None
 
 def main():
     start = time.perf_counter()
-    visited = replace_all_icons()
+    lst = walk_directory()
+    replace_all_icons(lst)
     end = time.perf_counter()
-    print(f"Visisted {visited} files. Finished in {round(end - start, 2)} seconds.")
+    print(f"{end-start}")
 
 if __name__ == "__main__":
     main()
