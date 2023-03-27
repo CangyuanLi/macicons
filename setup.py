dc1b639da5e335793285cab49681e9c8edf5b3c8
CangyuanLi <everest229@gmail.com>
2023-03-27 15:10:03 -0400

    version 0.0.8

--- setup.py
+++ setup.py
@@ -2,23 +2,19 @@ import setuptools
 
 setuptools.setup(
     name="macicons",
-    version="0.0.7",
+    version="0.0.8",
     author="Cangyuan Li",
     author_email="everest229@gmail.com",
     description="CLI utility to change Mac Icons.",
     url="https://github.com/CangyuanLi/macicons",
     packages=["macicons"],
     include_package_data=True,
-    entry_points={
-        "console_scripts": [
-            "macicons=macicons.cli:main"
-        ]
-    },
+    entry_points={"console_scripts": ["macicons=macicons.cli:main"]},
     install_requires=["tqdm", "pyobjc-core", "pyobjc-framework-Cocoa"],
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: MacOS"
-    ]
+        "Operating System :: MacOS",
+    ],
 )
