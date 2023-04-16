7d22e8fbb53b74dd6ae155e41336da9f1dc42636
CangyuanLi <everest229@gmail.com>
2023-04-16 16:03:38 -0400

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="macicons",
-    version="0.1.0",
+    version="0.1.1",
     author="Cangyuan Li",
     author_email="everest229@gmail.com",
     description="CLI utility to change Mac Icons.",
